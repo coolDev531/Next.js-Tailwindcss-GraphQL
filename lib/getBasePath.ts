3bd2aad5643c36baf8d72c1cc8981015c3685d01
HaradaYasuo <academic.dev0531@gmail.com>
2023-05-06 19:21:47 -0400

    update getBasePath

--- lib/getBasePath.ts
+++ lib/getBasePath.ts
@@ -2,7 +2,9 @@ const getBasePath = () => {
   let base_url =
     process.env.NODE_ENV === 'development'
       ? 'http://localhost:3000'
-      : `https://${process.env.VERCEL_URL}`;
+      : `https://${
+          process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL
+        }`;
 
   return base_url;
 };
