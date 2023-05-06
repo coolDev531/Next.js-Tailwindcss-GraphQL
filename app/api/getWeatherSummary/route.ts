ce3c398f9a7ce090bb0b2fd8d2e17711507bb693
HaradaYasuo <academic.dev0531@gmail.com>
2023-05-06 16:33:04 -0400

    make api endpoint that calls chatgpt and returns in to the weather page

---
+++ app/api/getWeatherSummary/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+import openai from '@/openai';
+
+export async function POST(request: Request) {
+  const { weatherData } = await request.json();
+
+  const response = await openai.createChatCompletion({
+    model: 'gpt-4',
+    temperature: 0.8,
+    n: 1,
+    stream: false,
+    messages: [
+      {
+        role: 'system',
+        content: `Pretend you're a weather news presenter presenting LIVE on television. be energetic and full of charisma. Introduce yourself as Atmos and say you are LIVE from the AtmosTrend Headquarters. State the city you are providing a summary for. Then give a summary of todays weather only. Make it easy for the viewer to understand and know what to do to prepare for those weather conditions such as wear SPF if the UV is high etc. use the uv_index data provided to provide UV advice. Provide a joke regarding the weather. Assume the data came from your team at the news office and not the user.`,
+      },
+      {
+        role: 'user',
+        content: `Hi there, can i get a summary of todays weather, use the following information to get the weather data: ${JSON.stringify(
+          weatherData
+        )}`,
+      },
+    ],
+  });
+
+  const { data } = response;
+  return NextResponse.json(data.choices[0].message);
+}
