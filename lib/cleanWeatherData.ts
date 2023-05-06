d45653c94ccf3d918312809af392b223b0839dc4
HaradaYasuo <academic.dev0531@gmail.com>
2023-05-06 16:32:33 -0400

    create clean weather data func

---
+++ lib/cleanWeatherData.ts
@@ -0,0 +1,48 @@
+import { Root } from '@/types/weather';
+
+const cleanWeatherData = (data: Root, city: string) => {
+  const {
+    current_weather,
+    timezone,
+    hourly,
+    hourly_units,
+    timezone_abbreviation,
+  } = data;
+
+  const { temperature, windspeed, winddirection, weathercode, time } =
+    current_weather;
+
+  const {
+    temperature_2m,
+    snowfall,
+    rain,
+    relativehumidity_2m,
+    precipitation_probability,
+    uv_index,
+  } = hourly;
+
+  return {
+    current_weather: {
+      temperature,
+      windspeed,
+      winddirection,
+      time,
+      weathercode,
+    },
+    hourly: {
+      // slicing only the first 24 hours worth of data so chatgpt doesn't charge me for too much data (tokens)
+      temperature_2m: temperature_2m.slice(0, 24),
+      snowfall: snowfall.slice(0, 24),
+      rain: rain.slice(0, 24),
+      relativehumidity_2m: relativehumidity_2m.slice(0, 24),
+      precipitation_probability: precipitation_probability.slice(0, 24),
+      uv_index: uv_index.slice(0, 24),
+    },
+    timezone,
+    timezone_abbreviation,
+    hourly_units,
+    city,
+  };
+};
+
+export default cleanWeatherData;
